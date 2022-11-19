7337f5d70afdbfad59613068ee16c5d7fadaebe5
nikkikhou <nikkikhou@gmail.com>
2022-11-18 19:38:40 -0500

    got signup and login fully functional

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,34 @@
 const router = require("express").Router();
 const { Post, Comment, User } = require("../models");
+const withAuth = require('../utils/auth');
 
-// get all
-router.get('/', async (req, res) => {
-// post findAll
-// map through data, serialize
-// render appropriate view
+// // get all
+// router.get('/', async (req, res) => {
+// // post findAll
+// // map through data, serialize
+// // render appropriate view
 
+// });
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id 
+            },
+            include: [ { model: User } ]
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 
